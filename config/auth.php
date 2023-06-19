beed2c4b3d33016a18250508eeef693ab802c645
YazidAufar <yazid.aufar.ya@gmail.com>
2023-06-19 13:32:53 +0800

    API new

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api', // <== update ini
         'passwords' => 'users',
     ],
 
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'token', // <== pastikan ini
             'provider' => 'users',
             'hash' => false,
         ],
